4dad8ec2d0945d165f86b633f5b31782f8d5e73d
Raytesnel <raymondhouwing@gmail.com>
2023-12-08 16:09:03 +0100

    2b done

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,43 @@ fn main() {
             // println!("game is not possible")
         }
     }
-    println!("score: {:?}",total_score)
+    println!("score: {:?} (1b)",total_score);
+    // opdracht b
+    let mut totalnumber:u32=0 ;
+    for (index,line) in file_contents.lines().enumerate(){
+        let cleaned_string = clean_string(line);
+        let subset_number = calc_minimum_set(cleaned_string);
+        totalnumber += subset_number;
+    }
+    println!("total number = {:?} (2b)",totalnumber)
 
+}
+
+
+fn calc_minimum_set(cleaned_string:String)->u32{
+    // find the minimum and multiply them
+    let items: Vec<&str> = cleaned_string.split(" ").filter(|&s| !s.is_empty()).collect();
+    let mut temp_block:HashMap<&str,u32> = HashMap::new();
+    temp_block.insert("red", 0);
+    temp_block.insert("blue", 0);
+    temp_block.insert("green", 0);
+    for chunk in items.chunks(2) {
+        if let [number, block] = chunk {
+            if let (Ok(parsed_number), Some(&dict_number)) = (
+                number.parse::<u32>(),
+                temp_block.get(block)
+            ){
+                if dict_number < parsed_number{
+                    temp_block.insert(block,parsed_number);
+                }
+            }
+
+        }
+    }
+    // println!("max numbers = {:?}",temp_block);
+    let total_number = temp_block.values().fold(1, |acc, &value| acc * value);
+    // println!("counted = {:?}",total_number);
+    return total_number
 }
 
 fn check_string(predict_dict:&HashMap<&'static str, u32>,cleaned_string:String)->bool{
@@ -43,6 +78,7 @@ fn check_string(predict_dict:&HashMap<&'static str, u32>,cleaned_string:String)-
 }
 
 fn setup()->HashMap<&'static str,u32>{
+    // makes the given dict where we need to below
     let mut predicted_blocks_dict:HashMap<&'static str,u32> = HashMap::new();
     predicted_blocks_dict.insert("red", 12);
     predicted_blocks_dict.insert("blue", 14);
@@ -59,6 +95,7 @@ fn clean_string(lines: &str)->String{
 }
 
 fn read_file() ->String{
+    //read the file returns a really long string
    let file_contents = fs::read_to_string("src/input_data/input_day_2_a.txt")
         .expect("LogRocket: Should have been able to read the file{}");
     return file_contents
