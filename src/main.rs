4ae2971c0621fbc169dedebc4bfdf12cc5a8b625
Raytesnel <raymondhouwing@gmail.com>
2023-12-08 15:22:57 +0100

    2a done

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,61 @@
 use std::fs;
+use std::collections::HashMap;
+
 
 fn main() {
-    clean_string_to_list();
+    let predicted_blocks_dict: HashMap<&'static str, u32> = setup();
+    let file_contents = read_file();
+    let mut total_score:u32 = 0;
+    for (index,line) in file_contents.lines().enumerate(){
+        let clean_string = clean_string(line);
+        if check_string(&predicted_blocks_dict,clean_string){
+            total_score += (index+1) as u32;
+            // println!("game is possible")
+        }
+        else{
+            // println!("game is not possible")
+        }
+    }
+    println!("score: {:?}",total_score)
+
 }
 
-fn clean_string_to_list(){
-    let mut result: Vec<&str> = Vec::new();
-    for line in read_file().lines(){
-        let line_trimmed: String = line.split(':').nth(1).unwrap_or("").trim().to_string();
-        let line_splitted:Vec<&str>=line_trimmed.split("; ").collect();
-        let mut numbers_per_game:Vec<i32> = Vec::new();
-        numbers_per_game.push(0);
-        numbers_per_game.push(0);
-        numbers_per_game.push(0);
-        for item in line_splitted {
-            let sub_result: Vec<&str> = item.split(", ").collect();
-            let mut highest_game_number:Vec<i32> = Vec::new();
-            for blocks in sub_result{
-                let collected_blocks: Vec<&str> =blocks.split(" ").collect();
-                let string_number  = collected_blocks.get(0).unwrap().trim();
-                if let Ok(parsed_number) = string_number.parse::<i32>() {
-                    // Successfully parsed, now 'parsed_number' is an i32
-                    highest_game_number.push(parsed_number);
-                } else {
-                    // Failed to parse, handle the error or provide a default value
-                    println!("Failed to parse as i32");
+fn check_string(predict_dict:&HashMap<&'static str, u32>,cleaned_string:String)->bool{
+    let items: Vec<&str> = cleaned_string.split(" ").filter(|&s| !s.is_empty()).collect();
+    for chunk in items.chunks(2) {
+        if let [number, block] = chunk {
+            // println!("this number{:?} and this block{:?}", number, block);
+            if let (Ok(parsed_number), Some(&dict_number)) = (
+                number.parse::<u32>(),
+                predict_dict.get(block)
+            ){
+                // Successfully parsed, now 'parsed_number' is an i32
+                if dict_number < parsed_number{
+                    return false
                 }
+            } else {
+                // Failed to parse, handle the error or provide a default value
+                println!("Failed to parse as u32");
             }
-            println!("{:?}",highest_game_number);
-            for (index,item) in highest_game_number.iter().enumerate() {
-                if highest_game_number.get(index) > numbers_per_game.get(index) {
-                    if let Some(value) = highest_game_number.get(index) {
-                        let extracted_value: i32 = *value;
-                        println!("{:?} is higher then {:?}", highest_game_number.get(index), numbers_per_game.get(index));
-
-                        //replace the item
-                        if let Some(existing_value) = numbers_per_game.get_mut(index) {
-                            *existing_value = extracted_value;
-                            println!("Updated vector: {:?}", numbers_per_game);
-                        } else {
-                            println!("Index out of bounds");
-                        }                    }
-                }
-            }
-            println!("total : {:?}",numbers_per_game)
-            // result.extend(sub_result);
         }
-
     }
+    return true
+}
+
+fn setup()->HashMap<&'static str,u32>{
+    let mut predicted_blocks_dict:HashMap<&'static str,u32> = HashMap::new();
+    predicted_blocks_dict.insert("red", 12);
+    predicted_blocks_dict.insert("blue", 14);
+    predicted_blocks_dict.insert("green", 13);
+    return predicted_blocks_dict
+}
+
+fn clean_string(lines: &str)->String{
+    // removes the game session & quotes, so it returns a vector with pairs of 2
+    let line_trimmed: String = lines.split(':').nth(1).unwrap_or("").trim().to_string();
+    let clean_string = line_trimmed.replace(";","").replace(":","").replace(",","");
+    // println!("cleaned string is : {clean_string}");
+    return clean_string
 }
 
 fn read_file() ->String{
