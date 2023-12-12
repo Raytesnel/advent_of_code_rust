caaf1aee7b17f07b8f2fa6cc364c1bcc223fa8dc
Raytesnel <raymondhouwing@gmail.com>
2023-12-12 23:31:37 +0100

    refactored

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,3 +1,6 @@
+use std::iter::{Enumerate, Peekable};
+use std::str::Chars;
+
 pub fn day_3_a(file_contents:String) {
     let file_list = file_contents.lines();
     let mut total_sum:u32 = 0;
@@ -5,41 +8,49 @@ pub fn day_3_a(file_contents:String) {
         let mut chars_iter = file_line.chars().enumerate().peekable();
         while let Some((index_char, file_item)) = chars_iter.next() {
             if is_digit(file_item){
-
-                // Let's get all numbers around the symbol
-                let mut numbers_around_symbol = String::new();
-                let mut index_number: u32 =index_char as u32;
-                numbers_around_symbol.push(file_item);
-                while let Some((next_index, next_item)) = chars_iter.peek() {
-                    if next_item.is_digit(10) {
-                        numbers_around_symbol.push(*next_item);
-                        index_number = *next_index as u32;
-                        chars_iter.next(); // Move the iterator forward
-                    } else {
-                        break;
-                    }
-                }
-                // println!("found number from index: {:?} till {:?} with number{:?}",index_char,index_number, numbers_around_symbol);
-
+                let ( numbers_around_symbol, index_number) = get_all_number_series(index_char, file_item, &mut chars_iter);
                 // now we need to check if there is a symbol around the number series
-                if let Some(_symbol_index) = find_symbol_around_numbers(&file_contents, index_char, index_number as usize,index_row) {
-                    // let's add them to to totals
-                    println!("yes symbol found at row: {:?} at chars{:?} till {:?} with number{:?}",index_row,index_char,index_number, numbers_around_symbol);
-
-                    if let Ok(combined_integer) = numbers_around_symbol.parse::<u32>() {
-                        total_sum += combined_integer;
-                    } else {
-                        println!("Failed to convert to u32");
+                match check_symbol_around(&file_contents, index_char, index_number as usize, index_row, numbers_around_symbol){
+                    Ok(result)=>{
+                        total_sum += result
                     }
-
-                }
-                else{
-                    println!("no symbol found at row: {:?} at chars{:?} till {:?} with number{:?}",index_row,index_char,index_number, numbers_around_symbol);
+                    Err(error)=>{
+                        if !error.contains("Symbol not found"){
+                            println!("Error: {}", error);
+                        }
+                    }
                 }
             }
         }
     }
     println!("3-A: total number = {:?}",total_sum)
+}
+
+fn check_symbol_around(file_contents:&String, index_char:usize, index_number:usize, index_row:usize, numbers_around_symbol:String) ->Result<u32,String>{
+    if let Some(_symbol_index) = find_symbol_around_numbers(&file_contents, index_char, index_number,index_row) {
+        if let Ok(combined_integer) = numbers_around_symbol.parse::<u32>() {
+            return Ok(combined_integer);
+        } else {
+            return Err("Failed to convert to u32".to_string());
+        }
+    }
+    Err("Symbol not found".to_string())
+}
+
+fn get_all_number_series(index_char:usize,file_item:char,chars_iter:&mut Peekable<Enumerate<Chars<'_>>>)->(String,u32){
+    let mut numbers_around_symbol = String::new();
+    let mut index_number: u32 =index_char as u32;
+    numbers_around_symbol.push(file_item);
+    while let Some((next_index, next_item)) = chars_iter.peek() {
+        if next_item.is_digit(10) {
+            numbers_around_symbol.push(*next_item);
+            index_number = *next_index as u32;
+            chars_iter.next(); // Move the iterator forward
+        } else {
+            break;
+        }
+    }
+    return(numbers_around_symbol,index_number)
 }
 
 fn find_symbol_around_numbers(file_list: &str, start_index: usize, end_index: usize,index_row:usize) -> Option<usize> {
