52449789dd178d79f7a3f1ce3d62e2e1f4bb8405
Raytesnel <raymondhouwing@gmail.com>
2024-01-05 15:54:09 +0100

    all functions, but not correct answere

--- src/day_5.rs
+++ src/day_5.rs
@@ -10,11 +10,21 @@ static SEED_ORDER: [&str; 7] = [
     "humidity-to-location",
 ];
 
+fn follow_seed(input_dict: &HashMap<String, Vec<Vec<u64>>>, seed_number: &u64) -> u64 {
+    let mut previous_number = *seed_number;
+    let mut next_number = 0;
+    for seed_item in SEED_ORDER {
+        next_number = get_destination_number(input_dict, &seed_item, &previous_number);
+        previous_number = next_number;
+    }
+    return next_number;
+}
+
 fn get_destination_number(
-    items_dict: &HashMap<String, Vec<Vec<u32>>>,
+    items_dict: &HashMap<String, Vec<Vec<u64>>>,
     key: &str,
-    number: u32,
-) -> u32 {
+    number: &u64,
+) -> u64 {
     match is_number_in_range(&items_dict, &key, number.clone()) {
         Ok(number_returned) => {
             if let Some(entry) = items_dict.get(key) {
@@ -25,36 +35,36 @@ fn get_destination_number(
                 return start_number_dest + delta;
             }
         }
-        Err(_) => return number,
+        Err(_) => return number.clone(),
     }
-    number
+    number.clone()
 }
 
 fn is_number_in_range(
-    items_dict: &HashMap<String, Vec<Vec<u32>>>,
+    items_dict: &HashMap<String, Vec<Vec<u64>>>,
     key: &str,
-    number: u32,
-) -> Result<u32, String> {
+    number: u64,
+) -> Result<u64, String> {
     if let Some(entry) = items_dict.get(key) {
         for (index, range) in entry.iter().enumerate() {
             let start = range[1];
             let end = range[1] + range[2] - 1;
 
             if number >= start && number <= end {
-                return Ok(index as u32); // Number is within the range
+                return Ok(index as u64); // Number is within the range
             }
         }
     }
     Err("nothing found".to_string()) // Number is not in any range
 }
 
-fn retrieve_seeds(line: &str) -> Result<Vec<u32>, &'static str> {
+fn retrieve_seeds(line: &str) -> Result<Vec<u64>, &'static str> {
     if let Some(first_line) = line.lines().next() {
         if first_line.contains("seeds") {
-            let seed_list: Vec<u32> = first_line
+            let seed_list: Vec<u64> = first_line
                 .split_whitespace()
                 .skip(1)
-                .map(|item| item.parse::<u32>().unwrap_or_default())
+                .map(|item| item.parse::<u64>().unwrap_or_default())
                 .collect();
             Ok(seed_list)
         } else {
@@ -65,15 +75,15 @@ fn retrieve_seeds(line: &str) -> Result<Vec<u32>, &'static str> {
     }
 }
 
-fn split_mapping_from_line(multiline_string: &String) -> HashMap<String, Vec<Vec<u32>>> {
+fn split_mapping_from_line(multiline_string: &String) -> HashMap<String, Vec<Vec<u64>>> {
     let lines = multiline_string.lines();
 
-    let mut block_list: Vec<Vec<u32>> = Vec::new();
+    let mut block_list: Vec<Vec<u64>> = Vec::new();
     let mut header: String = "".to_string();
-    let mut items_dict: HashMap<String, Vec<Vec<u32>>> = HashMap::new();
+    let mut items_dict: HashMap<String, Vec<Vec<u64>>> = HashMap::new();
 
     for line in lines.skip(2) {
-        if line == "        " {
+        if line == "" {
             items_dict.insert(header.clone(), block_list.clone());
             header.clear();
             block_list.clear(); // Clear block_list for the next section
@@ -81,14 +91,14 @@ fn split_mapping_from_line(multiline_string: &String) -> HashMap<String, Vec<Vec
             header = line.replace(" map:", "").replace(" ", "");
         } else {
             let temp_items: Vec<String> = line.split_whitespace().map(|s| s.to_string()).collect();
-            let block_item: Vec<u32> = temp_items
+            let block_item: Vec<u64> = temp_items
                 .iter()
-                .map(|item| item.parse::<u32>().unwrap_or_default())
+                .map(|item| item.parse::<u64>().unwrap_or_default())
                 .collect();
             block_list.push(block_item);
         }
     }
-    let mut updated_dict: HashMap<String, Vec<Vec<u32>>> = HashMap::new();
+    let mut updated_dict: HashMap<String, Vec<Vec<u64>>> = HashMap::new();
     for (key, values) in items_dict.clone().into_iter() {
         let mut sorted_values = values;
         sorted_values.sort_by_key(|inner_vec| inner_vec[1]);
@@ -98,10 +108,32 @@ fn split_mapping_from_line(multiline_string: &String) -> HashMap<String, Vec<Vec
     updated_dict
 }
 
-pub fn day_5_a(file_contents: String) {
-    split_mapping_from_line(&file_contents);
+fn find_lowest_number(seed_locations: HashMap<u64, u64>) -> Result<u64, String> {
+    if let Some(lowest_value) = seed_locations.values().cloned().min() {
+        Ok(lowest_value)
+    } else {
+        Err("Seed locations map is empty.".to_string())
+    }
+}
 
-    println!("hello dag 5 a")
+pub fn day_5_a(file_contents: String) -> u64 {
+    let mapped_input = split_mapping_from_line(&file_contents);
+    let mut seed_locations: HashMap<u64, u64> = HashMap::new();
+    match retrieve_seeds(&file_contents) {
+        Ok(seeds) => {
+            for seed in seeds {
+                seed_locations.insert(seed, follow_seed(&mapped_input, &seed));
+            }
+        }
+        Err(e) => panic!("couldn't retrieve seeds, error:{}", e),
+    }
+    match find_lowest_number(seed_locations) {
+        Ok(value) => {
+            println!("5-A: total number = {:?}", value);
+            value
+        }
+        Err(_) => panic!("cound' find lowest number"),
+    }
 }
 
 #[cfg(test)]
@@ -148,8 +180,8 @@ mod tests {
     }
 
     #[fixture]
-    fn example_input_split() -> HashMap<String, Vec<Vec<u32>>> {
-        let mut items_dict: HashMap<String, Vec<Vec<u32>>> = HashMap::new();
+    fn example_input_split() -> HashMap<String, Vec<Vec<u64>>> {
+        let mut items_dict: HashMap<String, Vec<Vec<u64>>> = HashMap::new();
         items_dict.insert(
             "seed-to-soil".to_string(),
             vec![vec![52, 50, 48], vec![50, 98, 2]],
@@ -189,7 +221,7 @@ mod tests {
     #[rstest]
     fn test_split_text_map(
         #[from(example_input_data)] input_data: String,
-        example_input_split: HashMap<String, Vec<Vec<u32>>>,
+        example_input_split: HashMap<String, Vec<Vec<u64>>>,
     ) {
         let result_split_map = split_mapping_from_line(&input_data);
         for key in result_split_map.keys() {
@@ -203,64 +235,83 @@ mod tests {
     }
 
     #[rstest]
-    fn test_if_number_is_in_range(example_input_split: HashMap<String, Vec<Vec<u32>>>) {
+    fn test_if_number_is_in_range(example_input_split: HashMap<String, Vec<Vec<u64>>>) {
         let key_to_find = "seed-to-soil";
-        let number: u32 = 60;
-        let number_found = is_number_in_range(&example_input_split, key_to_find, number);
+        let number: u64 = 60;
+        let _ = is_number_in_range(&example_input_split, key_to_find, number);
         let key_to_find = "seed-to-soil";
-        let number: u32 = 5;
+        let number: u64 = 5;
         match is_number_in_range(&example_input_split, key_to_find, number) {
             Ok(_) => assert!(false),
             Err(_) => assert!(true),
         }
         let key_to_find = "fertilizer-to-water";
-        let number: u32 = 60;
+        let number: u64 = 60;
         let number_found = is_number_in_range(&example_input_split, key_to_find, number);
         assert_eq!(number_found.unwrap(), 3);
         let key_to_find = "light-to-temperature";
-        let number: u32 = 64;
+        let number: u64 = 64;
         let number_found = is_number_in_range(&example_input_split, key_to_find, number);
         assert_eq!(number_found.unwrap(), 1)
     }
 
     #[rstest]
-    fn test_return_destination(example_input_split: HashMap<String, Vec<Vec<u32>>>) {
+    fn test_return_destination(example_input_split: HashMap<String, Vec<Vec<u64>>>) {
         let key_to_find = "fertilizer-to-water";
-        let number: u32 = 60;
-        let new_number = get_destination_number(&example_input_split, key_to_find, number);
+        let number: u64 = 60;
+        let new_number = get_destination_number(&example_input_split, key_to_find, &number);
         assert_eq!(new_number, 56);
-        let number: u32 = 70;
-        let new_number = get_destination_number(&example_input_split, key_to_find, number);
+        let number: u64 = 70;
+        let new_number = get_destination_number(&example_input_split, key_to_find, &number);
         assert_eq!(new_number, 70)
     }
 
-    // #[fixture]
-    // fn seed_locations() -> Vec<Vec<u32>> {
-    //     let seed_locations = vec![vec![79, 81], vec![14, 14], vec![55, 57], vec![13, 13]];
-    //     seed_locations
-    // }
+    #[fixture]
+    fn seed_locations() -> Vec<Vec<u64>> {
+        let seed_locations = vec![vec![79, 82], vec![14, 43], vec![55, 86], vec![13, 35]];
+        seed_locations
+    }
 
-    // #[rstest]
-    // fn test_find_seed_locations(
-    //     seed_locations: Vec<Vec<u32>>,
-    //     example_input_split: HashMap<String, Vec<Vec<u32>>>,
-    // ) {
-    //     for seed_combi in seed_locations {
-    //         let seed_number = seed_combi[0];
-    //         let result_seed_loctions = follow_seed(&example_input_split, &seed_number);
-    //         assert_eq!(result_seed_loctions, seed_combi[1])
-    //     }
-    // }
+    #[rstest]
+    fn test_find_seed_locations(
+        seed_locations: Vec<Vec<u64>>,
+        example_input_split: HashMap<String, Vec<Vec<u64>>>,
+    ) {
+        for seed_combi in seed_locations {
+            let seed_number = seed_combi[0];
+            let result_seed_loctions = follow_seed(&example_input_split, &seed_number);
+            assert_eq!(result_seed_loctions, seed_combi[1])
+        }
+    }
+
+    #[rstest]
+    fn test_find_lowest_number() {
+        let mut seed_dict: HashMap<u64, u64> = HashMap::new();
+        seed_dict.insert(79, 82);
+        seed_dict.insert(14, 43);
+        seed_dict.insert(55, 86);
+        seed_dict.insert(13, 35);
+        match find_lowest_number(seed_dict) {
+            Ok(value) => assert_eq!(value, 35),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[rstest]
+    fn test_full_day_5_a(#[from(example_input_data)] input_data: String) {
+        let lowest_number = day_5_a(input_data);
+        assert_eq!(lowest_number, 35)
+    }
 
     #[fixture]
-    fn example_input_seeds() -> Vec<u32> {
+    fn example_input_seeds() -> Vec<u64> {
         vec![79, 14, 55, 13]
     }
 
     #[rstest]
     fn test_retrieve_seeds(
         #[from(example_input_data)] input_data: String,
-        example_input_seeds: Vec<u32>,
+        example_input_seeds: Vec<u64>,
     ) {
         match retrieve_seeds(&input_data) {
             Ok(seed_list) => {
